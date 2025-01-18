cc9ed2d9db2c3ccf2887b8d1cc97ca952caae346
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2025-01-18 14:56:04 +0545

    re-enabling the temporary links to test.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function App() {
 
-  const [ developerMode , setDeveloperMode ] = useState(false);
+  const [ developerMode , setDeveloperMode ] = useState(true);
 
   return (
     <>
